fc71441c6977550e01afbcae392adcb0147ef80b
Sebastian Johnsson <sebastian@Sebastian-Johnssons-MacBook.local>
2010-09-09 16:38:31 +0200

    Added railtie

---
+++ lib/thinking_sphinx/deltas/delayed_delta/railtie.rb
@@ -0,0 +1,11 @@
+require 'rails'
+
+class Railtie < Rails::Railtie
+  initializer :after_initialize do
+    Delayed::Worker.backend = :active_record
+  end
+
+  rake_tasks do
+    load 'thinking_sphinx/deltas/delayed_delta/tasks.rb'
+  end
+end

09789df9caccce554f18752e4b625596f5a18e0f
Sebastian Johnsson <sebastian@Sebastian-Johnssons-MacBook.local>
2010-09-09 16:12:32 +0200

    d

--- lib/thinking_sphinx/deltas/delayed_delta.rb
+++ lib/thinking_sphinx/deltas/delayed_delta.rb
@@ -1,4 +1,6 @@
 require 'delayed_job'
+require 'delayed_job/delayed/backend/active_record'
+
 require 'thinking_sphinx'
 require 'thinking_sphinx/deltas/delayed_delta/delta_job'
 require 'thinking_sphinx/deltas/delayed_delta/flag_as_deleted_job'
